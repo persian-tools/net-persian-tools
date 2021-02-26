1e664447209ebafb324de94759fa5644588ec1ed
Mojtaba Khooryani <m.khooryani@outlook.com>
2021-02-26 18:00:32 +0330

    Number To Words Added.

---
+++ PersianTools/Modules/NumberToWords.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Text;
+
+namespace PersianTools.Modules
+{
+    public static class NumberToWords
+    {
+        private static readonly string[] UNITS_MAP = new[] { "صفر", "یک", "دو", "سه", "چهار", "پنج", "شش", "هفت", "هشت", "نه", "ده", "یازده", "دوازده", "سیزده", "چهارده", "پانزده", "شانزده", "هفده", "هجده", "نوزده" };
+        private static readonly string[] TENS_MAP = new[] { "صفر", "ده", "بیست", "سی", "چهل", "پنجاه", "شصت", "هفتاد", "هشتاد", "نود" };
+        private static readonly string[] HUNDREDS_MAP = new[] { "صفر", "صد", "دویست", "سیصد", "چهارصد", "پانصد", "ششصد", "هفتصد", "هشتصد", "نهصد" };
+        private static readonly Scale[] SCALES = new[]
+        {
+            new Scale("هزار"),
+            new Scale("میلیون"),
+            new Scale("میلیارد"),
+            new Scale("بیلیون"),
+            new Scale("بیلیارد"),
+            new Scale("تریلیون"),
+        };
+
+        public static string Convert(long number)
+        {
+            if (number == 0)
+            {
+                return "صفر";
+            }
+            if (number < 0)
+            {
+                return "منفی " + Convert(Math.Abs(number));
+            }
+
+            var words = new StringBuilder();
+
+            number = ConvertScales(number, words);
+            number = ConvertHundreds(number, words);
+            ConvertRest(number, words);
+
+            return words.ToString();
+        }
+
+        private static void ConvertRest(long number, StringBuilder words)
+        {
+            if (number > 0)
+            {
+                if (words.Length > 0)
+                {
+                    words.Append(" و ");
+                }
+
+                if (number < 20)
+                {
+                    words.Append(UNITS_MAP[number]);
+                }
+                else
+                {
+                    words.Append(TENS_MAP[number / 10]);
+                    if ((number % 10) > 0)
+                    {
+                        words.Append(" و " + UNITS_MAP[number % 10]);
+                    }
+                }
+            }
+        }
+
+        private static long ConvertHundreds(long number, StringBuilder words)
+        {
+            if ((number / 100) > 0)
+            {
+                if (words.Length > 0)
+                {
+                    words.Append(" و ");
+                }
+
+                words.Append(HUNDREDS_MAP[number / 100]);
+                number %= 100;
+            }
+
+            return number;
+        }
+
+        private static long ConvertScales(long number, StringBuilder words)
+        {
+            for (int i = SCALES.Length - 1; i >= 0; i--)
+            {
+                var scale = SCALES[i];
+                var start = (long)Math.Pow(10, (i + 1) * 3);
+                if ((number / start) > 0)
+                {
+                    if (words.Length > 0)
+                    {
+                        words.Append(" و ");
+                    }
+
+                    words.Append(Convert(number / start) + $" {scale.Name}");
+                    number %= start;
+                }
+            }
+
+            return number;
+        }
+
+        private class Scale
+        {
+            public Scale(string name)
+            {
+                Name = name;
+            }
+            public string Name { get; private set; }
+        }
+    }
+}
---
+++ PersianToolsTests/NumberToWordsTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using PersianTools.Modules;
+using Xunit;
+
+namespace PersianToolsTests
+{
+    [ExcludeFromCodeCoverage]
+    public class NumberToWordsTests
+    {
+        [Theory]
+        [InlineData(0L, "صفر")]
+        [InlineData(1L, "یک")]
+        [InlineData(12L, "دوازده")]
+        [InlineData(100L, "صد")]
+        [InlineData(101L, "صد و یک")]
+        [InlineData(-1000L, "منفی یک هزار")]
+        [InlineData(1_000_000_000, "یک میلیارد")]
+        [InlineData(9_000_000_000_000_000_000, "نه تریلیون")]
+        [InlineData(9_000_000_000_001_000_000, "نه تریلیون و یک میلیون")]
+        [InlineData(9_000_000_000_000_000_001, "نه تریلیون و یک")]
+        [InlineData(2_147_483_647, "دو میلیارد و صد و چهل و هفت میلیون و چهارصد و هشتاد و سه هزار و ششصد و چهل و هفت")]
+        [InlineData(-2_147_483_648, "منفی دو میلیارد و صد و چهل و هفت میلیون و چهارصد و هشتاد و سه هزار و ششصد و چهل و هشت")]
+        [InlineData(9_223_372_036_854_775_807, "نه تریلیون و دویست و بیست و سه بیلیارد و سیصد و هفتاد و دو بیلیون و سی و شش میلیارد و هشتصد و پنجاه و چهار میلیون و هفتصد و هفتاد و پنج هزار و هشتصد و هفت")]
+        public void ToEnglishDigits_Returns_Expected_String_Successfully(long number, string expected)
+        {
+            Assert.Equal(expected, NumberToWords.Convert(number));
+        }
+    }
+}
