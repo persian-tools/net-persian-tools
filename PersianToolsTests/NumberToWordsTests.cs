9d62c810a4400e99731d92b69c1c46976b5965cc
Mojtaba Khooryani <m.khooryani@outlook.com>
2021-02-26 18:02:52 +0330

    small refactor

--- PersianToolsTests/NumberToWordsTests.cs
+++ PersianToolsTests/NumberToWordsTests.cs
@@ -21,7 +21,7 @@ namespace PersianToolsTests
         [InlineData(2_147_483_647, "دو میلیارد و صد و چهل و هفت میلیون و چهارصد و هشتاد و سه هزار و ششصد و چهل و هفت")]
         [InlineData(-2_147_483_648, "منفی دو میلیارد و صد و چهل و هفت میلیون و چهارصد و هشتاد و سه هزار و ششصد و چهل و هشت")]
         [InlineData(9_223_372_036_854_775_807, "نه تریلیون و دویست و بیست و سه بیلیارد و سیصد و هفتاد و دو بیلیون و سی و شش میلیارد و هشتصد و پنجاه و چهار میلیون و هفتصد و هفتاد و پنج هزار و هشتصد و هفت")]
-        public void ToEnglishDigits_Returns_Expected_String_Successfully(long number, string expected)
+        public void Convert_Returns_Expected_String_Successfully(long number, string expected)
         {
             Assert.Equal(expected, NumberToWords.Convert(number));
         }
