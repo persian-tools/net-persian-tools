eba42d92e14d147ce78c5128c1cf35ad4a658fc2
Mojtaba Khooryani <m.khooryani@outlook.com>
2021-02-27 13:04:03 +0330

    Add WOrdsToNumber Module

---
+++ PersianTools/Modules/WordsToNumber.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PersianTools.Modules
+{
+    public static class WordsToNumber
+    {
+        private static readonly Dictionary<string, long> WORDS_NUMBER_MAP = new Dictionary<string, long>()
+        {
+            { "صفر", 0}, { "یک", 1 }, { "دو", 2 }, { "سه", 3 }, { "چهار", 4 }, { "پنج",5},{"شش",6},{"شیش",6},
+            {"هفت",7},{"هشت",8},{"نه",9},{"ده",10},{"یازده",11},{"دوازده",12},
+            {"سیزده",13},{"چهارده",14},{"پانزده",15},{"شانزده",16},{"هفده",17},
+            {"هجده",18},{"نوزده",19},{"بیست",20},{"سی",30},{"چهل",40},
+            {"پنجاه",50},{"شصت",60},{"هفتاد",70},{"هشتاد",80},{"نود",90},
+            {"صد",100},{"دویست",200},{"سیصد",300},{"چهارصد",400},{"پانصد",500},{"ششصد",600},
+            {"شیشصد",600},{"هفتصد",700},{"هشتصد",800},{"نهصد",900},
+            {"هزار",1_000},{"میلیون",1_000_000},
+            {"میلیارد",1_000_000_000},{"بیلیون",1_000_000_000_000},{"بیلیارد",1_000_000_000_000_000},
+            {"تریلیون",1_000_000_000_000_000_000}
+        };
+
+        public static long Convert(string numberString)
+        {
+            if (string.IsNullOrWhiteSpace(numberString))
+            {
+                throw new System.ArgumentException($"'{nameof(numberString)}' cannot be null or whitespace", nameof(numberString));
+            }
+
+            string input = numberString.Replace(" و ", " ");
+            var numbers = Regex.Matches(input, @"\w+")
+                    .Cast<Match>()
+                    .Select(m => m.Value)
+                    .Where(v => WORDS_NUMBER_MAP.ContainsKey(v))
+                    .Select(v => WORDS_NUMBER_MAP[v])
+                    .ToArray();
+            long acc = 0, total = 0L;
+            foreach (var n in numbers)
+            {
+                if (n >= 1000)
+                {
+                    total += acc * n;
+                    acc = 0;
+                }
+                else
+                {
+                    acc += n;
+                }
+            }
+            return (total + acc) * (numberString.StartsWith("منفی") ? -1 : 1);
+        }
+    }
+}
---
+++ PersianToolsTests/WordsToNumberTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using PersianTools.Modules;
+using Xunit;
+
+namespace PersianToolsTests
+{
+    [ExcludeFromCodeCoverage]
+    public class WordsToNumberTests
+    {
+        [Fact]
+        public void Convert_Throw_ArgumentException_When_Input_Is_Null()
+        {
+            Action act = () => WordsToNumber.Convert(null);
+
+            var exception = Assert.Throws<ArgumentException>(act);
+        }
+
+        [Fact]
+        public void Convert_Throw_ArgumentException_When_Input_Is_Default()
+        {
+            Action act = () => WordsToNumber.Convert(default);
+
+            var exception = Assert.Throws<ArgumentException>(act);
+        }
+
+        [Fact]
+        public void Convert_Throw_ArgumentException_When_Input_Is_WhiteSpacec()
+        {
+            Action act = () => WordsToNumber.Convert(" ");
+
+            var exception = Assert.Throws<ArgumentException>(act);
+        }
+
+        [Theory]
+        [InlineData("صفر", 0L)]
+        [InlineData("یک", 1L)]
+        [InlineData("دوازده", 12L)]
+        [InlineData("صد", 100L)]
+        [InlineData("یک هزار و یک", 1001L)]
+        [InlineData("صد و یک", 101L)]
+        [InlineData("منفی یک هزار", -1000L)]
+        [InlineData("یک میلیارد", 1_000_000_000L)]
+        [InlineData("نه تریلیون", 9_000_000_000_000_000_000L)]
+        [InlineData("نه تریلیون و یک میلیون", 9_000_000_000_001_000_000L)]
+        [InlineData("نه تریلیون و یک", 9_000_000_000_000_000_001L)]
+        [InlineData("دو میلیارد و صد و چهل و هفت میلیون و چهارصد و هشتاد و سه هزار و ششصد و چهل و هفت", 2_147_483_647L)]
+        [InlineData("منفی دو میلیارد و صد و چهل و هفت میلیون و چهارصد و هشتاد و سه هزار و ششصد و چهل و هشت", -2_147_483_648L)]
+        [InlineData("نه تریلیون و دویست و بیست و سه بیلیارد و سیصد و هفتاد و دو بیلیون و سی و شش میلیارد و هشتصد و پنجاه و چهار میلیون و هفتصد و هفتاد و پنج هزار و هشتصد و هفت", 9_223_372_036_854_775_807L)]
+        public void Convert_Returns_Expected_String_Successfully(string numberInWords, long expected)
+        {
+            Assert.Equal(expected, WordsToNumber.Convert(numberInWords));
+        }
+    }
+}
