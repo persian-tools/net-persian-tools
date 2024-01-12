bfeeb48a335e38eaf697273cc763a36ee9349d88
alireza_sariri <alirezasariri78@gmail.com>
2024-01-12 15:20:10 +0330

    create sort text

---
+++ PersianTools/Modules/SortText.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PersianTools.Modules
+{
+    public static class SortText
+    {
+        public static string[] Sort(string text)
+        => Sort(text.Split(' '));
+        public static string[] Sort(params string[] texts)
+        => texts.OrderBy(c => c).ToArray();
+    }
+}
