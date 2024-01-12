cc967deffa0a33d08187f72b938062ecba656688
alireza_sariri <alirezasariri78@gmail.com>
2024-01-12 22:56:23 +0330

    add legalId Verify for c#

---
+++ PersianTools/Modules/LegalId.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace PersianTools.Modules
+{
+    public static class LegalId
+    {
+        public static bool Verify(string legalId)
+        {
+            const int legalLength = 11;
+            if (legalId.Length > legalLength || legalId.Length == 0) return false;
+            // check string has no illegal char
+            if (legalId.Any(c => !int.TryParse(c.ToString(), out int res))) return false;
+
+            int controller = legalId[legalId.Length - 1].ToInt();
+            int m = legalId[legalId.Length - 2].ToInt() + 2;
+            int[] z = { 29, 27, 23, 19, 17 };
+            int sum = 0;
+            for (int i = 0; i < legalId.Length - 1; i++)
+            {
+                sum += z[i % 5] * (legalId[i].ToInt() + m);
+            }
+            sum %= 11;
+            if (sum == 10) sum = 0;
+            return sum == controller;
+        }
+        private static int ToInt(this char c)
+            => int.Parse(c.ToString());
+    }
+}
---
+++ PersianToolsTests/LegalIdTest.cs
@@ -0,0 +1,26 @@
+﻿using PersianTools.Modules;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Xunit;
+
+namespace PersianToolsTests
+{
+    [ExcludeFromCodeCoverage]
+    public class LegalIdTest
+    {
+        [Theory]
+        [InlineData("123000000", false)]
+        [InlineData("11111111111", false)]
+        [InlineData("10380284792", false)]
+        [InlineData("10380285692", false)]
+        [InlineData("09748208301", false)]
+        [InlineData("10380284790", true)]
+
+        public void Check_Legal(string input, bool expected)
+        {
+            Assert.Equal(expected, LegalId.Verify(input));
+        }
+    }
+}
